6764f57dba8300f021e4a44e65b6e5704d7e0741
Vibhav0 <vibhav.surve@xaviers.edu.in>
2022-05-26 12:48:15 +0530

    Improved error handling

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ use std::str::FromStr;
 use std::sync::mpsc::{channel, Sender};
 use std::thread;
 use std::time::Duration;
-const MAX: u16 = 1000;
+
+// Total number of ports in system
+const MAX: u16 = 65535;
 #[derive(Debug)]
 pub struct Arguments {
     pub flag: String,
@@ -19,28 +21,66 @@ impl Arguments {
     fn new(mut args: env::Args) -> Result<Arguments, &'static str> {
         //Firtst args.next() is to skip the target path
         args.next();
-        let flags = args.next().unwrap();
-        // args.next() returns an Option type and to extract value from it we use unwrap method and it returns String type
-        if let Ok(ip) = IpAddr::from_str(args.next().unwrap().as_str()) {
-            return Ok(Arguments {
-                flag: String::from(flags),
-                ip_address: ip,
-                thread: 10000,
-            });
-        };
-        Err("Invalid IP Address")
+        let flags = args.next();
+        match flags {
+            Some(flag) => {
+                if flag.trim().contains("-t") {
+                    let threads_or_ip = args.next();
+                    let threads_or_ip_clone = threads_or_ip.clone();
+                    match threads_or_ip.unwrap().parse::<u16>() {
+                        Ok(num_threads) => {
+                            let number_of_threads: u16 =
+                                args.next().unwrap().trim().parse::<u16>().unwrap();
+                            // args.next() returns an Option type and to extract value from it we use unwrap method and it returns String type
+                            if let Ok(ip) = IpAddr::from_str(args.next().unwrap().as_str()) {
+                                return Ok(Arguments {
+                                    flag: String::from(flag),
+                                    ip_address: ip,
+                                    thread: number_of_threads,
+                                });
+                            };
+                        }
+                        _ => {
+                            if let Ok(ip) = IpAddr::from_str(threads_or_ip_clone.unwrap().as_str())
+                            {
+                                return Ok(Arguments {
+                                    flag: String::from(flag),
+                                    ip_address: ip,
+                                    thread: 100,
+                                });
+                            };
+                        }
+                    }
+                } else {
+                    return Err("Use -t flag for threads");
+                }
+            }
+            _ => {
+                args.next();
+                if let Ok(ip) = IpAddr::from_str(args.next().unwrap().as_str()) {
+                    return Ok(Arguments {
+                        flag: String::from(""),
+                        ip_address: ip,
+                        thread: 100,
+                    });
+                };
+            }
+        }
+        Err("Something went wrong!!")
     }
 
     fn scan(tx: Sender<u16>, start_port: u16, ip: IpAddr, num_threads: u16) {
         let mut port: u16 = start_port + 1;
-
-        if TcpStream::connect_timeout(&SocketAddr::new(ip, port), Duration::from_secs(1)).is_ok() {
-            println!(".");
-            //Not completely understood what flush does
-            //https://doc.rust-lang.org/std/io/trait.Write.html#tymethod.flush
-            io::stdout().flush().unwrap();
-            tx.send(port).unwrap();
-
+        while (MAX - port) >= num_threads {
+            if TcpStream::connect_timeout(&SocketAddr::new(ip, port), Duration::from_secs(1))
+                .is_ok()
+            {
+                print!(".");
+                //Not completely understood what flush does
+                //https://doc.rust-lang.org/std/io/trait.Write.html#tymethod.flush
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
             port += num_threads;
         }
     }
@@ -48,7 +88,7 @@ impl Arguments {
 
 pub fn run(args: env::Args) {
     let arguments = Arguments::new(args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments {}", err);
+        println!("Invalid arguments {}", err);
         process::exit(1);
     });
     let num_threads = arguments.thread;
@@ -67,8 +107,10 @@ pub fn run(args: env::Args) {
         open_ports.push(port);
     }
     println!();
-    open_ports.sort_unstable();
+    open_ports.sort();
     for port in open_ports {
         println!("Port {} is open", port);
     }
 }
+
+// If we have to pass in the flags like '-j' or '-some_flag' it should always precede with '--'
